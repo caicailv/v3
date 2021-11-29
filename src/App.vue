7191ca1e701a52fb7a1b2a591f48bf8a2c4fd414
caicailv <1401394657@qq.com>
2021-11-29 15:20:35 +0800

    c

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Rate from './components/Rate.vue';
+import { useFullscreen } from '@vueuse/core';
+const { isFullscreen, enter, exit, toggle } = useFullscreen();
+</script>
+<template>
+	<h1 @click="toggle">click</h1>
+	<Rate theme="#fff000" val="3" />
+
+</template>
---
+++ src/components/Rate.vue
@@ -0,0 +1,37 @@
+<!-- 组件间传值 -->
+<template>
+	<p class="rate" @mouseout="mouseout">
+		<span class="rate_list" v-for="i in 5" @click="rateNum = i">
+			<span v-if="i < rateNum + 1">★</span>
+			<span v-else>☆</span>
+		</span>
+	</p>
+</template>
+<script setup>
+import { ref, watch } from 'vue';
+let { val, theme } = defineProps({
+	val: {
+		type: Number,
+		default: 2,
+	},
+	theme: {
+		type: String,
+		default: 'red',
+	},
+});
+let rateNum = ref(val);
+watch(rateNum, (e) => {
+	console.log('e', e);
+	defineEmits();
+});
+</script>
+<style scoped>
+.rate {
+	color: v-bind(theme);
+}
+.rate span {
+	font-size: 20px;
+	display: inline-block;
+	margin-left: 5px;
+}
+</style>
